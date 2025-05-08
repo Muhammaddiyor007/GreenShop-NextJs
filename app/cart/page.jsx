c699b3b2d8beadd32541d1dd46b14c25a51777e6
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2025-05-08 18:57:05 +0500

    "Green-shop"

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,35 +1,46 @@
+'use client'
+
 import React from 'react'
 import CartTotals from './totals'
-import CartItem from './team'
 import HeaderSm from '../Components/header/Responsive/headerSm'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeFromCart } from '../redux/features/cart/cartSlice'
 
-const CartPage = async () => {
-  const res = await fetch('https://dummyjson.com/carts/5')
-  const data = await res.json()
-
-  const products = data?.products || []
-  console.log('products:', products)
+const ShoppingPage = () => {
+  const cartItems = useSelector((state) => state.cart.items)
+  const dispatch = useDispatch()
 
   return (
     <div className="max-w-[1200px] mx-auto px-[15px] mt-[50px]">
-			<div className="md:hidden">
-      <HeaderSm/>
+      <div className="md:hidden">
+        <HeaderSm />
       </div>
       <h1 className="text-2xl font-semibold mb-6">Shopping Cart</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="md:col-span-2 space-y-4">
-          {products.length === 0 ? (
+          {cartItems.length === 0 ? (
             <p className="text-gray-500">Cart is empty.</p>
           ) : (
-            products.map((product) => (
-              <CartItem key={product.id} product={product} />
+            cartItems.map((item) => (
+              <div key={item.id} className="border p-4 rounded-md flex justify-between items-center">
+                <div>
+                  <p className="font-semibold">{item.title}</p>
+                  <p>${item.price}</p>
+                </div>
+                <button
+                  onClick={() => dispatch(removeFromCart(item.id))}
+                  className="text-red-600 font-bold"
+                >
+                  Remove
+                </button>
+              </div>
             ))
           )}
         </div>
-        <CartTotals products={products} />
+        <CartTotals products={cartItems} />
       </div>
     </div>
   )
 }
 
-export default CartPage
+export default ShoppingPage
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Slices/AuthSlice';
+import cartReducer from './features/cart/cartSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-
+    cart: cartReducer,
   },
 });
